ff0d8a2489c2a8932181017335abaa80948a8c4b
magma <tanayachyute@gmail.com>
2023-05-05 16:53:03 +0530

    complete exercises 02 and 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
 
+    let output = ``;
+    
+    if(num === 0 || word === ``)   return output;
+
+    for(let i = 0; i <= num-1; i++)
+    {
+        output+=word;
+    }
+    return output || "ERROR";
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,5 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    return word.split(/(?!$)/u).reverse().join(``);
 };
 
 // Do not edit below this line
