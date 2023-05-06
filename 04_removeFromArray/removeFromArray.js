99e1f1347eaebffe64f524aee66027ecf03d4a32
magma <tanayachyute@gmail.com>
2023-05-06 16:36:33 +0530

    complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+    let newarr = [];
+    
+    for(let e of nums)
+    {
+        for(let i = 0; i < arr.length; i++)
+        {
+            if(e === arr[i])
+            {
+                arr.splice(i,1);
+            }
+        }
+    }
+   
+    return arr;
 
 };
+console.log(removeFromArray([1,2,3,4],3,2));
 
 // Do not edit below this line
 module.exports = removeFromArray;
