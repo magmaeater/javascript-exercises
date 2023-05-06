a543184de13bd1255017ad71bf614d14d8a8b1ec
magma <tanayachyute@gmail.com>
2023-05-06 16:53:32 +0530

    complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+const sumAll = function(n1,n2) {
+    sum = 0;
+    
+    if(n1 < 0 || n2 < 0 || !Number.isInteger(n1) || !Number.isInteger(n2))   return "ERROR";
 
+    for(let i = Math.min(n1,n2); i <= Math.max(n1,n2); i++)
+    {
+    sum+=i;
+    }
+    return sum || "ERROR";
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
